5b13676ace9e9eb8ebc4a52ecb8c21f4a735fe76
wafash08 <saefulhaqwafa@gmail.com>
2025-01-29 15:10:19 +0700

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+	.get("/", () => {
+		const data = "data";
+		return "Hello Elysia CI/CD";
+	})
+	.listen(3000);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+	`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
 );
